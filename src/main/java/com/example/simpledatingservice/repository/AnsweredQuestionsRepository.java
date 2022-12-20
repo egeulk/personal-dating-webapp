4f0d6b9781ac4f41e8fe12c6d445ef81ef217435
egeulk <egeulker99@gmail.com>
2022-12-20 18:47:22 +0300

    fix showing of unanswered questions

--- src/main/java/com/example/simpledatingservice/repository/AnsweredQuestionsRepository.java
+++ src/main/java/com/example/simpledatingservice/repository/AnsweredQuestionsRepository.java
@@ -1,19 +1,17 @@
 package com.example.simpledatingservice.repository;
 
 import com.example.simpledatingservice.entities.*;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AnsweredQuestionsRepository extends CrudRepository<AnsweredQuestion, Long> {
-    @Query(value = "SELECT q FROM Question q, AnsweredQuestion aq JOIN aq.user u " +
-            "WHERE q.id NOT IN   (SELECT q.id FROM q, aq JOIN aq.user u WHERE u.id = :userId)")
+    @Query(value = "SELECT q FROM Question q WHERE NOT EXISTS (SELECT aq from AnsweredQuestion aq join aq.user u where u.id=:userId AND aq.question.id=q.id) ")
     List<Question> findNonAnsweredQuestions(@Param("userId") Long userId);
 
+//    @Query(value = "SELECT q FROM Question q WHERE NOT EXISTS (SELECT aq FROM AnsweredQuestion aq JOIN aq.user u, aq.question.id aq-id WHERE u.id = :userId AND aq-id=q.id )")
     @Query(value = "SELECT aq.answer FROM AnsweredQuestion aq WHERE aq.user.id = :userId AND aq.question.id=:questionId")
     Answer findAnswerByUserAndQuestionId(@Param("userId") Long userId, @Param("questionId") Long questionId);
 
